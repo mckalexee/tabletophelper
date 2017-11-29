52c23dbed58343a04fe05d39c191e75b6613308a
Alex McKenzie <alex@unbreak-it.com>
2017-11-28 23:55:22 -0600

    Navbar (#3)
    * Added navbar
    
    * Added dev deployment to dev.helper.gbase.io
    
    * Fixed issue with mobile address bars not going away.

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tt-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  isCollapsed = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
